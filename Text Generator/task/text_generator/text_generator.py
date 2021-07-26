cab1117fac7c0292cfef697b4d56c4a39b47a1da
kesava Reddy <kesav4u@gmail.com>
2021-07-25 19:46:46 -0500

    better approach

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -14,27 +14,20 @@ with open(input(), 'r', encoding='utf-8') as file:
 trigrams = list(zip(tokens, tokens[1:], tokens[2:]))
 
 for h1, h2, t in trigrams:
-    trigrams_dict[h1 + " " + h2][t] += 1
+    trigrams_dict[(h1, h2)][t] += 1
 
 heads = list(trigrams_dict.keys())
 
 first_word_set = set()
 while len(first_word_set) < 10:
     word = choice(heads)
-    if word[0].isupper() and word.split()[0][-1] not in ".!?" and word[-1] not in ".!?":
+    if word[0][0].isupper() and word[0][-1] not in ".!?" and word[-1] not in ".!?":
         first_word_set.add(word)
 
 for word in first_word_set:
-    sentence = [word]
-    while len(sentence) < 4 or sentence[-1][-1] not in ".!?":
-        if len(sentence) <= 1:
-            next_head = sentence[-1]
-        elif len(sentence) == 2:
-            next_head = " ".join([sentence[-2].split()[1], sentence[-1]])
-        else:
-            next_head = " ".join(sentence[-2::])
-
-        next_items = trigrams_dict.get(next_head).most_common()
+    sentence = [word[0], word[1]]
+    while len(sentence) < 5 or sentence[-1][-1] not in ".!?":
+        next_items = trigrams_dict.get((sentence[-2], sentence[-1])).most_common()
         next_words = [w[0] for w in next_items]
         next_words_count = [c[1] for c in next_items]
         sentence.extend(choices(next_words, weights=next_words_count))
